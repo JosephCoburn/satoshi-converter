efd6164d2549c9773e7cad5f502ef168b5afd664
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-08-23 16:35:22 -0700

    fix math

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById("btcInput").addEventListener("input", function(e) {
   var btc = e.target.value;
-  document.getElementById("satoshiOutput").innerHTML = (btc*100000000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  document.getElementById("microbitOutput").innerHTML = (btc*1000000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  document.getElementById("millibitOutput").innerHTML = (btc*1000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  document.getElementById("centibitOutput").innerHTML = (btc*100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  document.getElementById("satoshiOutput").innerHTML = (btc/0.00000001).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  document.getElementById("microbitOutput").innerHTML = (btc/0.000001).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  document.getElementById("millibitOutput").innerHTML = (btc/0.001).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  document.getElementById("centibitOutput").innerHTML = (btc/0.01).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 });
