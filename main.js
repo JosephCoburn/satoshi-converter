2050fdc84f0c8b6583e9d56e141a067eae861d73
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-08-26 14:13:41 -0700

    MVP

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-document.getElementById("btcInput").addEventListener("input", function(e) {
-  var btc = e.target.value;
-  if (btc) {
-    document.getElementById("satoshiOutput").innerHTML = "ok";
-    document.getElementById("microbitOutput").innerHTML = (btc/0.000001).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    document.getElementById("millibitOutput").innerHTML = (btc/0.001).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    document.getElementById("centibitOutput").innerHTML = (btc/0.01).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    document.getElementById("BTCOutput").innerHTML = (btc).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
-});
+document.getElementById("valInput").addEventListener("input", function(e) {
+  var val = e.target.value;
 
-document.getElementById("selectBTC").addEventListener("click", function(e) {
-  document.getElementById("dropdownMenuButton").innerHTML = "BTC";
-  console.log("BTC selected");
-});
-
-document.getElementById("selectMBTC").addEventListener("click", function(e) {
-  document.getElementById("dropdownMenuButton").innerHTML = "mBTC";
-  console.log("mBTC selected");
-});
-
-document.getElementById("selectSatoshis").addEventListener("click", function(e) {
-  document.getElementById("dropdownMenuButton").innerHTML = "Satoshis";
-  console.log("sats selected");
+  document.getElementById("selectBTC").addEventListener("click", function() {
+    document.getElementById("dropdownMenuButton").innerHTML = "BTC";
+    if (val) {
+      console.log("BTC val = ", val)
+      document.getElementById("satoshiOutput").innerHTML = (val*100000000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      document.getElementById("milibitOutput").innerHTML = (val*1000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      document.getElementById("BTCOutput").innerHTML = (val).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  });
+  
+  document.getElementById("selectMBTC").addEventListener("click", function() {
+    document.getElementById("dropdownMenuButton").innerHTML = "mBTC";
+    if (val) {
+      console.log("mBTC val = ", val)
+      document.getElementById("satoshiOutput").innerHTML = (val*100000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      document.getElementById("milibitOutput").innerHTML = (val).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      document.getElementById("BTCOutput").innerHTML = (val*0.001).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  });
+  
+  document.getElementById("selectSatoshis").addEventListener("click", function() {
+    document.getElementById("dropdownMenuButton").innerHTML = "Satoshi";
+    if (val) {
+      console.log("satoshi val = ", val)
+      document.getElementById("satoshiOutput").innerHTML = (val).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      document.getElementById("milibitOutput").innerHTML = (val*0.00001).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      document.getElementById("BTCOutput").innerHTML = (val*0.00000001).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  });
 });
